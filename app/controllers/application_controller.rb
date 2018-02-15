54e53d225db2c7776c333e6839162059ae7d6fca
Marko Milovanovic <markomilovanovic24@gmail.com>
2018-02-15 03:46:10 +0000

    Adding photo grid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,31 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  require "open-uri"
+  require "json"
+  
   def homesweethome
+    #require 'open-url'
+    
+    instagram = open("https://www.instagram.com/picsbymilo/?__a=1")
+    
+    doc = ""
+    instagram.each do |line|
+      doc << line
+    end
+    instagram.close
+
+    resp = JSON.parse(doc)
+    
+    @image_links = Array.new
+    @image_captions = Array.new
+    
+    nodes =  resp['user']['media']['nodes']
+    
+    nodes.each do |node|
+      @image_links.push(node['display_src'])
+      @image_captions.push(node['caption'])
+    end
+    
   end
 end
